690f690da2a62fca2919ff0f16d1fe9b83c8bc1a
Kodai <kodai8250@gmail.com>
2022-10-16 17:54:01 +0900

    commit

---
+++ src/component/App/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+import * as S from './style';
+import { AppIcon } from '../AppIcon';
+
+export const App = () => {
+  //配列の作成
+   const appIconList = [
+    {title:'youtube',icon:'',url:''},
+    {title:'facebook',icon:'',url:''},
+    {title:'amazon',icon:'',url:''}
+  ];
+  return (
+    <S.Container>
+      {appIconList.map((appIconItem) => {
+        return (
+          <AppIcon 
+           title={appIconItem.title}
+           icon={appIconItem.icon}
+           url={appIconItem.url}
+           />
+        )})}
+    </S.Container>
+  );
+}
