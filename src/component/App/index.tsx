8769cae9005d509d917cae5ce6f18abb56309f43
Kodai <kodai8250@gmail.com>
2022-10-22 07:30:36 +0900

    commit 5

--- src/component/App/index.tsx
+++ src/component/App/index.tsx
@@ -6,15 +6,21 @@ import { AppIcon } from '../AppIcon';
 export const App = () => {
   //配列の作成
    const appIconList = [
-    {title:'youtube',icon:'',url:''},
+    {title:'youtube',icon:'',url:'https://www.youtube.com/'},
     {title:'facebook',icon:'',url:''},
-    {title:'amazon',icon:'',url:''}
+    {title:'amazon',icon:'',url:''},
+    {title:'twitter',icon:'',url:''},
+    {title:'memo',icon:'',url:''},
+    {title:'zoom',icon:'',url:''},
+    {title:'instagram',icon:'',url:''},
+    {title:'Maps',icon:'',url:''},
+    {title:'iTunes',icon:'',url:''}
   ];
   return (
     <S.Container>
       {appIconList.map((appIconItem) => {
         return (
-        //propsを渡す
+          //todo propsでユーザーが設定したアプリタイトル・アイコン画像・アプリURLを渡す。
           <AppIcon 
            title={appIconItem.title}
            icon={appIconItem.icon}
--- src/component/AppIcon/index.tsx
+++ src/component/AppIcon/index.tsx
@@ -1,19 +1,26 @@
 import React from "react"
 import styled from "styled-components"
 
-const Container = styled.div`
+const IconImage = styled.img`
     width:  73px;
     height: 73px;
-    background-color: red;
+    border-radius: 5px;
+`;
+const URL = styled.a`
+    margin: 2% 5%;
+    text-decoration: none;
+    color: black;
+`;
+const Title = styled.p`
+    margin-top: 0;
+    text-align: center;
 `;
 
 //propsを受け取る。
 export const AppIcon: React.FC<{ title: string; icon: string; url:string }>  = (props) => {
-    return <div className="appicon">
-        <Container />
-        <img src={props.icon} />
-        <a href={props.url} />
-        <p>{props.title}</p>
-    </div>
+    return <URL href={props.url} target="_blank">
+        <IconImage src={props.icon} />
+        <Title>{props.title}</Title>
+        </URL>
         
 }
