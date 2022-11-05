0e4283cf114de2f5d0fc0e744e3d6ed62ff5fc79
Keyhoh <iheyhoh+github@gmail.com>
2022-11-05 16:24:50 +0900

    add: event

--- src/component/AddDialog/index.tsx
+++ src/component/AddDialog/index.tsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, {ChangeEventHandler, FormEventHandler, useCallback, useState} from 'react';
 import {Dialog} from '@mui/material';
+import {Bookmark} from '../context/type';
 
 type Props = {
   open: boolean
   onClose: () => void
 }
 
-export const AddDialog: React.FC<Props> = ({open, onClose}) => <Dialog open={open} onClose={onClose}>
-  dialog
-</Dialog>
+export const AddDialog: React.FC<Props> = ({open, onClose}) => {
+  const [bookmark, setBookmark] = useState<Bookmark>({
+    name: '',
+    url: '',
+    icon: '',
+  })
+  const setName: ChangeEventHandler<HTMLInputElement> = useCallback((e) => setBookmark({
+    ...bookmark,
+    name: e.target.value
+  }), [bookmark, setBookmark])
+  const setUrl: ChangeEventHandler<HTMLInputElement> = useCallback((e) => setBookmark({
+    ...bookmark,
+    url: e.target.value
+  }), [bookmark, setBookmark])
+  const handleSubmit: FormEventHandler = useCallback((e) => {
+    e.preventDefault()
+    console.log(bookmark)
+    onClose()
+  }, [bookmark, onClose])
+  return <Dialog open={open} onClose={onClose} onSubmit={handleSubmit}>
+    <form action='#' method='get'>
+      <input type='text' onChange={setName}/>
+      <input type='text' onChange={setUrl}/>
+      <button type='submit'>submit</button>
+    </form>
+  </Dialog>
+}
