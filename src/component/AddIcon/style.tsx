38734f119607d78d398e1ba49aa07c017b1b5ef7
Keyhoh <iheyhoh+github@gmail.com>
2022-11-12 13:40:11 +0900

    update: add icon

--- src/component/AddIcon/style.tsx
+++ src/component/AddIcon/style.tsx
@@ -11,15 +11,6 @@ export const Container = styled.div`
   gap: 5px;
 `;
 
-export const Icon = styled.div`
-  width: 72px;
-  height: 72px;
-  border-radius: 50%;
-  background-color: #fff;
-  -webkit-box-shadow: 0 3px 5px rgba(0, 0, 0, .3);
-  box-shadow: 0 3px 5px rgba(0, 0, 0, .3);
-`;
-
 export const Title = styled.p`
   margin: 0;
   text-align: center;
