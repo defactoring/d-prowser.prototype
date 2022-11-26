1a721ac88af1c3f268967849d0851a7af3afdc02
kodai-Java <111653799+kodai-Java@users.noreply.github.com>
2022-11-26 13:15:53 +0900

    Update src/component/NormalContents/index.tsx
    Co-authored-by: Ikeda <32242766+Keyhoh@users.noreply.github.com>

--- src/component/NormalContents/index.tsx
+++ src/component/NormalContents/index.tsx
@@ -2,7 +2,9 @@ import {Contents} from './Contents';
 import {AddDialog} from './AddDialog';
 import {useCallback, useState} from 'react';
 
-// 編集ボタン
+/**
+ * 通常モード
+ */
 export const NormalContents = () => {
   const [open, setOpen] = useState<boolean>(false);
   const handleOpen = useCallback(() => setOpen(true), [setOpen])
