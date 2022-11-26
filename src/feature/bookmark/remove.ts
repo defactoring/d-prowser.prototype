86fd91086cea08fa782a92339ae9d9ad374ee789
kodai-Java <111653799+kodai-Java@users.noreply.github.com>
2022-11-26 13:07:57 +0900

    Update src/feature/bookmark/remove.ts
    Co-authored-by: Ikeda <32242766+Keyhoh@users.noreply.github.com>

--- src/feature/bookmark/remove.ts
+++ src/feature/bookmark/remove.ts
@@ -1,7 +1,9 @@
 import {Bookmark} from './type';
 import {get} from './get';
 
-// ブックマーク削除
+/**
+ * ブックマーク削除
+ */
 export const remove = (id: Bookmark['id']) => {
   // 引数のブックマークIDを除いたブックマーク配列を変数に格納
   const bookmarks = get().filter(bookmark => bookmark.id !== id)
