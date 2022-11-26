4b8df865976c3edb9faa863ac6023a553320a18e
kodai-Java <111653799+kodai-Java@users.noreply.github.com>
2022-11-26 13:07:04 +0900

    Update src/component/EditContents/BackIcon/index.tsx
    Co-authored-by: Ikeda <32242766+Keyhoh@users.noreply.github.com>

--- src/component/EditContents/BackIcon/index.tsx
+++ src/component/EditContents/BackIcon/index.tsx
@@ -3,8 +3,10 @@ import * as S from './style';
 import {Tooltip} from '@mui/material';
 import {useMode} from '../../../hooks';
 
-// 戻るボタン
-// 編集モードからメイン画面に戻る。
+/**
+ * 戻るボタン
+ * 編集モードからメイン画面に戻る。
+ */
 export const BackIcon: React.FC = () => {
   const {setMode} = useMode()
   const handleClick = useCallback(() => setMode('normal'), [setMode])
