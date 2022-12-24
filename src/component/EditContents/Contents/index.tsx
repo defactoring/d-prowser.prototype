2e7a1ac87bbe14041ea442f8c9d7a55ffafd3dd5
Kodai <kodai8250@gmail.com>
2022-12-24 11:02:37 +0900

    タイトル編集機能追加

--- src/component/EditContents/Contents/index.tsx
+++ src/component/EditContents/Contents/index.tsx
@@ -1,7 +1,7 @@
 import React, {useCallback, useState} from 'react';
 import * as S from './style';
 import {BackIcon} from '../BackIcon';
-import {get} from '../../../feature/bookmark';
+import {Bookmark, get} from '../../../feature/bookmark';
 import {AppIcon} from '../AppIcon';
 import {InitializeIcon} from '../InitializeIcon';
 
@@ -10,7 +10,9 @@ import {InitializeIcon} from '../InitializeIcon';
  * 初期化ボタン, 戻るボタン, アプリアイコン
  */
 export const Contents = () => {
-  const [bookmarks, setBookmarks] = useState(get())
+  // ブックマーク配列を取り出しstateとして定義
+  const [bookmarks, setBookmarks] = useState<Bookmark[]>(get())
+  // ブックマーク配列をリフレッシュされたときに更新する関数を定義
   const handleRefresh = useCallback(() => setBookmarks(get()), [setBookmarks])
   return (
     <S.Container>
