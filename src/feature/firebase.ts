65af9c99dd3d34cf491f601953ffab83cb0492a4
Kodai <kodai8250@gmail.com>
2023-01-01 13:58:02 +0900

    Firebase データ追加検索処理の追加

--- src/feature/firebase.ts
+++ src/feature/firebase.ts
@@ -1,13 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getDatabase, ref, set } from "firebase/database";
+import { getFirestore, collection, getDocs, snapshotEqual } from "firebase/firestore";
 import { Bookmark } from './bookmark/type'
+import { doc, setDoc, getDoc } from "firebase/firestore"; 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// firebase設定
 const firebaseConfig = {
   apiKey: "AIzaSyAP9LewVkw8TGZVDXZz9fyvKL8HkOki5KI",
   authDomain: "d-prowser.firebaseapp.com",
@@ -16,21 +15,32 @@ const firebaseConfig = {
   messagingSenderId: "627346574140",
   appId: "1:627346574140:web:7249e68584307147cfac39",
   measurementId: "G-N30LQ2PYRT",
-  databaseURL: "https://d-prowser-default-rtdb.firebaseio.com"
+  //databaseURL: "https://d-prowser-default-rtdb.firebaseio.com"
 };
 
-// Initialize Firebase
+// Firebase初期化
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
-const database = getDatabase();
+const db = getFirestore(app);
 
+/**
+ * ブックマークをDBに登録
+ */
 export const writeBookmarkData = (bookmark: Bookmark) => {
-  const db = getDatabase(app);
-  set(ref(db, 'Bookmark/' + bookmark.id), {
-    id: bookmark.id,
-    title: bookmark.title,
-    url: bookmark.url,
-    icon: bookmark.icon
-  });
+    setDoc(doc(db, "bookmarks", bookmark.id), {
+        id: bookmark.id,
+        title: bookmark.title,
+        url: bookmark.url,
+        icon: bookmark.icon
+    })
+}
+/**
+ * DBからブックマーク配列を取得
+ * @returns 
+ */
+export const getBookmarkData = (): Bookmark[] => {
+    const bookmarks = getDocs(collection(db, "bookmarks")).then((snapshot) => 
+    snapshot.docs.map(doc => doc.data())
+    )
+    return bookmarks;// ブックマークの配列をDBから取得する方法が不明です。。
 }
