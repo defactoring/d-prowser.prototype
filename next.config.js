e6d5163d19c06337ef1c0ce7f6eb93c94da4bb71
Keyhoh <iheyhoh+github@gmail.com>
2023-04-29 15:46:19 +0900

    add: firebase hosting deploy

--- next.config.js
+++ next.config.js
@@ -5,7 +5,6 @@ const withPWA = require('next-pwa')({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/d-prowser.prototype',
   reactStrictMode: true,
   swcMinify: true,
   compiler: { emotion: true },
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,12 +13,12 @@ const App = ({ Component, pageProps }: AppProps) => {
     <>
       <Head>
         <meta charSet='utf-8' />
-        <link rel='icon' href='/d-prowser.prototype/favicon.ico' />
+        <link rel='icon' href='/favicon.ico' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <meta name='theme-color' content='#000000' />
         <meta name='description' content='d-prowser' />
-        <link rel='apple-touch-icon' href='/d-prowser.prototype/logo192.png' />
-        <link rel='manifest' href='/d-prowser.prototype/manifest.json' />
+        <link rel='apple-touch-icon' href='/logo192.png' />
+        <link rel='manifest' href='/manifest.json' />
         <title>d-prowser</title>
       </Head>
       <ThemeProvider theme={theme}>
